6fe7167c09b2e8f499ddf146d1cb3af4d84f3aa1
Trong Hieu <tronghieu.designer@gmail.com>
2021-01-09 18:54:08 +0700

    - Remove all fragments to redesign app

--- app/src/main/java/com/example/notes/NavigationActivity.java
+++ app/src/main/java/com/example/notes/NavigationActivity.java
@@ -1,9 +1,12 @@
 package com.example.notes;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -12,6 +15,23 @@ import androidx.navigation.ui.NavigationUI;
 
 public class NavigationActivity extends AppCompatActivity {
 
+    private TextView mTextMessage;
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener(){
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item){
+            switch(item.getItemId()){
+                case R.id.navigation_tasks:
+
+                    return true;
+                case R.id.navigation_notes:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +39,14 @@ public class NavigationActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+//        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.navigation_tasks, R.id.navigation_notes)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+//        NavigationUI.setupWithNavController(navView, navController);
+
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
 }
